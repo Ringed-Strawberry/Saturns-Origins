7318be28da44417ac3ff73db039f51c178659208
yey <adressemail1a2b@gmail.com>
2025-12-24 20:12:19 +0100

    Added json file for swarm

--- src/main/java/rings_of_saturn/github/io/saturns_origins/mixin/SwarmPlayerMixin.java
+++ src/main/java/rings_of_saturn/github/io/saturns_origins/mixin/SwarmPlayerMixin.java
@@ -10,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import rings_of_saturn.github.io.saturns_origins.util.OriginUtil;
 import rings_of_saturn.github.io.saturns_origins.util.ResourceUtil;
 
 @Mixin(value = PlayerEntity.class)
